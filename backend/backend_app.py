a09e071bcd4ec6a5fd320e29ee06e5eebe2eb611
Pooch41 <d.kulikovski@gmail.com>
2025-09-28 18:02:37 +0200

    backend_add post logic

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,25 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
-def get_posts():
-    return jsonify(POSTS)
+@app.route('/api/posts', methods=['GET','POST'])
+def post():
+    if request.method == "POST":
+        title = request.json['title']
+        content = request.json['content']
+        if not POSTS:
+            new_id = 1
+        else:
+            current_ids = [post['id'] for post in POSTS]
+            new_id = max(current_ids) + 1
+        new_post = {
+            "id": new_id,
+            "title": title,
+            "content": content
+        }
+        POSTS.append(new_post)
+        return jsonify(new_post), 201
+    else:
+        return jsonify(POSTS)
 
 
 if __name__ == '__main__':
